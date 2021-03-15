81248262a157687e184305882caefe00998542e7
Ponmo <77691125+Ponmo@users.noreply.github.com>
2021-03-15 09:44:29 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -8,6 +8,17 @@ app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  
 def render_main():
     return render_template('home.html')
 
+def main():
+    with open('county_demographics.json') as demographics_data:
+        counties = json.load(demographics_data)
+
+def get_state_options():
+    listOfStates = []
+    htmlcode = ""
+    for county in counties:
+        if county["State"] not in listOfStates:
+            state_dictionary.append(county["State"])
+    return 
 
 if __name__=="__main__":
     app.run(debug=False)
